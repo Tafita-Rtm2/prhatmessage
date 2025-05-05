73da30477922a201a9c808806451c66b46b31041
Tafitaniaina rtm <harenabainina@gmail.com>
2025-05-05 17:39:06 +0300

    Update home.js

--- public/home.js
+++ public/home.js
@@ -14,14 +14,16 @@ import {
 } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "VOTRE_API_KEY",
-  authDomain: "VOTRE_AUTH_DOMAIN",
-  databaseURL: "VOTRE_DATABASE_URL",
-  projectId: "VOTRE_PROJECT_ID",
-  storageBucket: "VOTRE_STORAGE_BUCKET",
-  messagingSenderId: "VOTRE_MESSAGING_SENDER_ID",
-  appId: "VOTRE_APP_ID"
+  apiKey: "AIzaSyCDbP-yKWXD4L1HowJt3ZSyEpMUMrzJZjI",
+  authDomain: "tafitafaceboot.firebaseapp.com",
+  databaseURL: "https://tafitafaceboot-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "tafitafaceboot",
+  storageBucket: "tafitafaceboot.firebasestorage.app",
+  messagingSenderId: "893787639070",
+  appId: "1:893787639070:web:e5dd2798f3cb2b6fa24d0f",
+  measurementId: "G-H64J1YYPYE"
 };
+
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
